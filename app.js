12af8c3b3f4b9f3db9c77df57f04025725573ade
HanoonIH <hanoonih123@gmail.com>
2020-11-24 22:54:23 +0530

    class images added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var hbs = require('express-handlebars');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -12,6 +14,15 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+app.engine('hbs', hbs(
+  {
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/layout/',
+    partialsDir: __dirname + '/views/partials/'
+  }
+));
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,55 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+const classData = [
+  {
+    class: '01',
+    subjectsCount: 5
+  },
+  {
+    class: '02',
+    subjectsCount: 3
+  },
+  {
+    class: '03',
+    subjectsCount: 4
+  },
+  {
+    class: '04',
+    subjectsCount: 5
+  },
+  {
+    class: '05',
+    subjectsCount: 3
+  },
+  {
+    class: 'TQ',
+    subjectsCount: 4
+  },
+  {
+    class: '06',
+    subjectsCount: 4
+  },
+  {
+    class: '07',
+    subjectsCount: 5
+  },
+  {
+    class: '08',
+    subjectsCount: 3
+  },
+  {
+    class: '09',
+    subjectsCount: 4
+  },
+  {
+    class: '10',
+    subjectsCount: 4
+  },
+];
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { classData });
 });
 
 module.exports = router;
