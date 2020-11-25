5264793915588dc21a97f1654650cbca6d0c5fa6
HanoonIH <hanoonih123@gmail.com>
2020-11-25 12:09:46 +0530

    class 1 arabi playlist test done

--- routes/index.js
+++ routes/index.js
@@ -5,46 +5,65 @@ var router = express.Router();
 const classData = [
   {
     class: '01',
+    subjects: [ 
+      { name: 'Islamika Balapadavali'},
+      { name: 'Arabic'}
+    ],
     subjectsCount: 5
   },
   {
     class: '02',
+    subjects: [ 
+      { name: 'Islamika Balapadavali'},
+      { name: 'Arabic'},
+      { name: 'Thilavathul Quran'},
+      { name: 'Tharjumathul Quran'}
+    ],
     subjectsCount: 3
   },
   {
     class: '03',
+    subjects: [],
     subjectsCount: 4
   },
   {
     class: '04',
+    subjects: [],
     subjectsCount: 5
   },
   {
     class: '05',
+    subjects: [],
     subjectsCount: 3
   },
   {
     class: 'TQ',
+    subjects: [],
     subjectsCount: 4
   },
   {
     class: '06',
+    subjects: [],
     subjectsCount: 4
   },
   {
     class: '07',
+    subjects: [],
     subjectsCount: 5
   },
   {
     class: '08',
+    subjects: [],
     subjectsCount: 3
   },
   {
     class: '09',
+    subjects: [],
     subjectsCount: 4
   },
   {
     class: '10',
+    subjects: [],
     subjectsCount: 4
   },
 ];
@@ -54,7 +73,11 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/class/:class', (req, res) => {
-  res.render('class');
+  let classSubjects = classData.filter((item) => {
+    return item.class == req.params.class
+  });
+  // console.log(classSubjects[0].subjects);
+  res.render(`class${req.params.class}`, { classSubs: classSubjects[0].subjects });
 })
 
 module.exports = router;
